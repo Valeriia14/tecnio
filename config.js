67b0f307d09155af37d18ac76731a4a86de1450e
Valeriia <valeriiadesign14@gmail.com>
2023-06-01 22:58:11 +0300

    fix the cors error

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
 export default {
+    CORS_URL:'https://cors-anywhere.herokuapp.com/',
     HOST_URL:'https://dev.app.tectio.net',
-    COOKIE: 'rremember_token="api-test-user-2@tectio.com|d83667120983c5ffdb169dd306feaeb0cbdb67c310193f75fffa2cbe20044a9f1466a7eed0f4999ca4ee6619f87979f82be5b2053d9a2b00ff8ed0cc73ca904c"; session=.eJwtzjEOwjAMQNG7ZKYoduIkZuImyHFs0QGB2jAh7g5IrF9_eK9w8c32azjN7WmHcFlHOAUR8OoWM6WE7EVjbNg9eq5NuBkQFmJKigIGKeXmwBQjA7bBWEtM5h4VKX9vLezULTOwFJI08nBQY6vdnQ2lJWUTEuijM2r18IU8d9v-mse6TNvn8ksLnqfpXO9Hvd_C-wOVbDjO.ZHg4-w.9wKeeMPvb3U0gtRl6R6L--4k9FI',
+    COOKIE: 'api-test-user-2@tectio.com|d83667120983c5ffdb169dd306feaeb0cbdb67c310193f75fffa2cbe20044a9f1466a7eed0f4999ca4ee6619f87979f82be5b2053d9a2b00ff8ed0cc73ca904c"; session=.eJwtzjEOwjAMQNG7ZKYoduIkZuImyHFs0QGB2jAh7g5IrF9_eK9w8c32azjN7WmHcFlHOAUR8OoWM6WE7EVjbNg9eq5NuBkQFmJKigIGKeXmwBQjA7bBWEtM5h4VKX9vLezULTOwFJI08nBQY6vdnQ2lJWUTEuijM2r18IU8d9v-mse6TNvn8ksLnqfpXO9Hvd_C-wOVbDjO.ZHjsbA.mpvsRMeEMrAOmC5OMEFe9Eo1d8Q',
+    USER_NAME:"api-test-user-2@tectio.com",
+    PASSWORD:"YY6HDzaciCUz"
 }
